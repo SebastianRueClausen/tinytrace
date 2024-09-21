679962c60b25a35f4923762991813f272f33eb5d
Sebastian Rue Clausen <seba474b@gmail.com>
2024-09-21 12:12:37 +0200

    Added missing file.

---
+++ src/includes/light_sampling.glsl
@@ -0,0 +1,8 @@
+#ifndef LIGHT_SAMPLING
+#define LIGHT_SAMPLING
+
+#define LightSampling uint
+const RestirReplay LIGHT_SAMPLING_NONE = 1;
+const RestirReplay LIGHT_SAMPLING_NEXT_EVENT_ESTIMATION = 2;
+
+#endif
